8cffcbdb0b8c889d599ce5257c3dc0e63bdd996f
cwt-cherry <159461017+cwt-cherry@users.noreply.github.com>
2024-03-16 17:00:02 +0000

    Initial commit

---
+++ assignment-5.cpp
@@ -0,0 +1,35 @@
+// PHYS 30762 Programming in C++
+// Assignment 5
+// Practice inheritance in C++ classes
+// See slides for guidance
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+
+using std::string;
+
+// Beginning of lepton class (hint: start from assignment 4 particle class)
+class lepton
+{
+private:
+  //...
+public:
+  //...
+protected: 
+  //...
+
+};
+// End of lepton class and associated member functions
+
+// Derived classes for other particles, and four-momentum class go here
+
+// Implementation of functions from a class should be done outside the class, preferably in another file
+
+// Main program
+int main()
+{
+  //...
+  return 0;
+}
