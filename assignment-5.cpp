b0aeae092181c8d272e3ff05a7414082a8e0babc
cwt_cherry <wing.chan-6@student.manchester.ac.uk>
2024-04-08 14:06:16 +0100

    four momentum class

--- assignment-5.cpp
+++ assignment-5.cpp
@@ -25,6 +25,36 @@ protected:
 
 // Derived classes for other particles, and four-momentum class go here
 
+class FourMomentum
+{
+  private:
+    double energy{0}; // Between 0 and c
+    double momentum_x{0};
+    double momentum_y{0};
+    double momentum_z{0};
+
+  public:
+    FourMomentum(): energy{}, momentum_x{}, momentum_y{}, momentum_z{}
+    {}
+
+    FourMomentum(double particle_energy, double particle_momentum_x, double particle_momentum_y, double particle_momentum_z):
+    energy{particle_energy}, momentum_x{particle_momentum_x}, momentum_y{particle_momentum_y}, momentum_z{particle_momentum_z}
+    {}
+
+  void set_energy(double particle_energy)
+  {
+    if(particle_energy >= 0) // not limiting energy to the speed of light due to change in slides
+    {
+      energy = particle_energy;
+    }
+    else
+    {
+      std::cerr<<"Invalid energy. Energy should be positive."<<std::endl;
+      exit(0);
+    }
+  }
+};
+
 // Implementation of functions from a class should be done outside the class, preferably in another file
 
 // Main program
