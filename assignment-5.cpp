0653dcacb3b12d4d83c41d351d6f0c697926a9e9
cwt_cherry <wing.chan-6@student.manchester.ac.uk>
2024-04-10 16:30:43 +0100

    tau

--- assignment-5.cpp
+++ assignment-5.cpp
@@ -42,6 +42,71 @@ class lepton
     // Destructor
     ~lepton() {std::cout<<"Destroying "<<name<<std::endl;}
 
+    // Setter functions
+    void set_name(string particle_name)
+    {
+      if(particle_name == "electron" || particle_name == "positron" || particle_name == "muon" || particle_name == "antimuon")
+      {
+        name = particle_name;
+      }
+      else
+      {
+        std::cerr << "Invalid particle name." << std::endl;
+        exit(0);
+      }
+    }
+
+    void set_rest_mass(double particle_rest_mass) {rest_mass = particle_rest_mass;}
+
+    void set_charge(int particle_charge)
+    {
+      if(particle_charge == 1 || particle_charge == -1)
+      {
+        charge = particle_charge;
+      }
+      else
+      {
+        std::cerr << "Invalid particle charge. Value should be either +1 or -1" << std::endl;
+        exit(0);
+      }
+    }
+
+    void set_velocity(double particle_velocity)
+    {
+      if(speed_of_light >= particle_velocity && particle_velocity >= 0)
+      {
+        velocity = particle_velocity;
+      }
+      else
+      {
+        std::cerr<<"Invalid velocity. Velocity should be between 0 and the speed of light."<<std::endl;
+        exit(0);
+      }
+    }
+
+    void set_beta(double particle_velocity)
+    {
+      if(speed_of_light >= particle_velocity && particle_velocity >= 0)
+      {
+        beta = particle_velocity/speed_of_light;
+      }
+      else
+      {
+        std::cerr<<"Invalid beta value. Value should be between 0 and 1."<<std::endl;
+        exit(0);
+      }
+    }
+
+    void set_antiparticle(bool particle_antiparticle) {antiparticle = particle_antiparticle;}
+
+    // Getter functions
+  string get_name() const {return name;}
+  double get_velocity() const {return velocity;}
+  double get_rest_mass() const {return rest_mass;}
+  int get_charge() const {return charge;}
+  double get_beta() const {return beta;}
+  bool get_antiparticle() const {return antiparticle;}
+
 };
 // End of lepton class and associated member functions
 
@@ -64,18 +129,21 @@ class electronClass: public lepton
     // parameterised constructor
     electronClass(string particle_name, double particle_rest_mass, int particle_charge, double particle_velocity, bool particle_antiparticle): 
     lepton(particle_name, particle_rest_mass, particle_charge, particle_velocity, particle_antiparticle) {}
-    
-    // Constructor for Electron
-    electronClass(): lepton("electron", 0.511, -1, random_value(), false) {}
 
-    // Constructor for Positron (antielectron)
+    // destructor
+    ~electronClass() {std::cout<<"Destroying "<<name<<std::endl;}
+
+    // Constructor for electron
+    static electronClass electron()
+    {
+      return electronClass("electron", 0.511, -1, random_value(), false);
+    }
+
+    // Constructor for positron (antielectron)
     static electronClass positron()
     {
       return electronClass("positron", 0.511, +1, random_value(), true);
     }
-
-    // destructor
-    ~electronClass() {std::cout<<"Destroying "<<name<<std::endl;}
 
     // data members (cpp file)
     // assume calorimeter has 4 layers, measure the energy a particle loses as it passes through the detector 
@@ -101,6 +169,9 @@ class muonClass: public lepton
     muonClass(string particle_name, double particle_rest_mass, int particle_charge, double particle_velocity, bool particle_antiparticle, bool particle_isolated_status): 
     lepton(particle_name, particle_rest_mass, particle_charge, particle_velocity, particle_antiparticle), isolated_status{particle_isolated_status} {}
     
+    // destructor
+    ~muonClass() {std::cout<<"Destroying "<<name<<std::endl;}
+
     // Constructor for muon
     muonClass(): lepton("muon", 105.6, -1, random_value(), false), isolated_status{false} {}
 
@@ -128,23 +199,93 @@ class muonClass: public lepton
 class tauClass: public lepton
 {
   private:
+    bool hadronic_decay;
+    bool leptonic_decay;
+    std::unique_ptr<lepton> decay_product_1; // Smart pointer for decay product 1
+    std::unique_ptr<tauClass> decay_product_2; // Smart pointer for decay product 2
+    std::unique_ptr<neutrinoClass> decay_product_3; // Smart pointer for decay product 3
+
   public:
 
     // default constructor
-    tauClass(): lepton{} {}
+    tauClass(): lepton{}, leptonic_decay{} {}
 
     // parameterised constructor
-    tauClass(string particle_name, double particle_rest_mass, int particle_charge, double particle_velocity, bool particle_antiparticle): 
-    lepton(particle_name, particle_rest_mass, particle_charge, particle_velocity, particle_antiparticle) {}
-    
+    tauClass(string particle_name, double particle_rest_mass, int particle_charge, double particle_velocity, bool particle_antiparticle, bool particle_leptonic_decay, bool particle_hadronic_decay): 
+    lepton(particle_name, particle_rest_mass, particle_charge, particle_velocity, particle_antiparticle), leptonic_decay{particle_leptonic_decay}, hadronic_decay{particle_hadronic_decay} {}
+
+    // destructor
+    ~tauClass() {std::cout<<"Destroying "<<name<<std::endl;} 
+
     // Constructor for tau
-    tauClass(): lepton("tau", 1777, -1, random_value(), false) {}
+    static tauClass tau()
+    {
+      return tauClass("antitau", 1777, -1, random_value(), false, true, false);
+    }
 
     // Constructor for antitau
-    static tauClass positron()
+    static tauClass antitau()
     {
-      return tauClass("antitau", 1777, +1, random_value(), true);
+      return tauClass("antitau", 1777, +1, random_value(), true, true, false);
     }
+
+    // Flag for hadronic decay
+    void set_hadronic_decay(bool decay)
+    {
+      hadronic_decay = decay;
+      if(hadronic_decay)
+      {
+        if(name=="tau")
+        {
+          std::cout<<"Tau undergoes hadronic decay into hadrons and a tau neutrino."<<std::endl;
+          // change tau (obj) into hadrons and tau neutrino (using the objects from derived class)
+        }
+        else if(name=="antitau")
+        {
+          std::cout<<"Antitau undergoes hadronic decay into hadrons and a antitau neutrino."<<std::endl;
+          // change the antitau object into hadrons and antitau neutrino (using the objects from derived class)
+        }
+        else
+        {
+          throw std::invalid_argument("Invalid decay status.");
+        }
+      }
+      else
+      {
+        throw std::invalid_argument("Tau/ antitau does not go through hadronic decay.");
+      }
+    }
+
+    void set_leptonic_decay(bool decay)
+    {
+      leptonic_decay =  decay;
+      if (leptonic_decay)
+      {
+        if (name=="tau")
+        {
+          // Smart pointers for decay products
+          decay_product_1 = std::make_unique<lepton>();
+          decay_product_2 = std::make_unique<tauClass>(tauClass::tau()); // Creating a tau object using the static member function
+          decay_product_3 = std::make_unique<neutrinoClass>();
+          decay_product_3->set_flavour(decay_product_1->get_name());
+          delete this;
+        }
+        else if (name=="antitau")
+        {
+          // Smart pointers for decay products
+          decay_product_1 = std::make_unique<lepton>();
+          decay_product_2 = std::make_unique<tauClass>(tauClass::antitau()); // Creating an antitau object using the static member function
+          decay_product_3 = std::make_unique<neutrinoClass>();
+          decay_product_3->set_flavour(decay_product_1->get_name());
+          delete this;
+        }
+        else
+        {
+          throw std::invalid_argument("Invalid decay status.");
+        }
+      }
+    }
+
 };
 
 // neutrinos class (derived class)
@@ -162,10 +303,26 @@ class neutrinoClass: public lepton
     neutrinoClass(string particle_name, double particle_rest_mass, int particle_charge, double particle_velocity, bool particle_antiparticle, string particle_flavour, bool particle_interaction): 
     lepton(particle_name, particle_rest_mass, particle_charge, particle_velocity, particle_antiparticle), flavour{particle_flavour}, hasInteracted{particle_interaction} {}
     
+    // destructor
+    ~neutrinoClass() {std::cout<<"Destroying "<<name<<std::endl;}
+
     // setter and getters
     void set_flavour(const string& particle_flavour)
     {
-        flavour = particle_flavour;
+      // Check if the provided flavour is valid
+      if(particle_flavour == "electron" || particle_flavour == "muon" || particle_flavour == "tau")
+      {
+        flavour = particle_flavour + " neutrino";
+      }
+      else if(particle_flavour == "positron" || particle_flavour == "antimuon" || particle_flavour == "antitau")
+      {
+        flavour = particle_flavour + " neutrino";
+      }
+      else
+      {
+        // If the provided flavour is not valid, set a default flavour
+        flavour = "unknown";
+      }
     }
 
     string get_flavour() const
@@ -187,13 +344,13 @@ class neutrinoClass: public lepton
     //neutrinoClass(): lepton("neutrino", 940.6, 0, random_value(), false), flavour(get_flavour), hasInteracted(get_interaction) {}
     static neutrinoClass neutrino()
     {
-      return neutrinoClass("neutrino", 940.6, 0, random_value(), false), flavour(get_flavour), hasInteracted(get_interaction);
+      return neutrinoClass("neutrino", 940.6, 0, random_value(), false, "unknown", false);
     }
 
-    // Constructor for Positron (antineutrinos)
+    // Constructor for antineutrinos
     static neutrinoClass antineutrino()
     {
-      return neutrinoClass("antineutrino", 940.6, 0, random_value(), true), flavour(get_flavour), hasInteracted(get_interaction);
+      return neutrinoClass("antineutrino", 940.6, 0, random_value(), true, "unknown", true);
     }
 
     // data member (cpp)
@@ -231,8 +388,7 @@ class FourMomentum
       }
       else
       {
-        std::cerr<<"Invalid energy. Energy should be positive."<<std::endl;
-        exit(0);
+        throw std::invalid_argument("Energy should be positive.");
       }
     }
 
@@ -245,6 +401,5 @@ class FourMomentum
 // Main program
 int main()
 {
-  //...
   return 0;
 }
